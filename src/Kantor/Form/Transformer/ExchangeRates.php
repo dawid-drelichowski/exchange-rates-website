982d55b3cbb58e877312231cd0d2ea376513d32f
Dawid Drelichowski <dawid.drelichowski@gmail.com>
2014-01-02 21:31:39 +0000

    Addition of unit tests of exchange rates transformer

--- src/Kantor/Form/Transformer/ExchangeRates.php
+++ src/Kantor/Form/Transformer/ExchangeRates.php
@@ -3,16 +3,32 @@
 namespace Kantor\Form\Transformer;
 
 use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
 
 class ExchangeRates implements DataTransformerInterface
 {
+    const RATES_KEY = 'rates';
+    
     public function transform($value)
     {
-        return array('rates' => $value);
+        return array(self::RATES_KEY => $value);
     }
     
     public function reverseTransform($value)
     {
-        return array_shift($value);
+        if (!is_array($value)) {
+            throw new TransformationFailedException(
+                'ExchangeRates reverse tranformation failed: value is not an array'
+            );
+        }
+        
+        if (!array_key_exists(self::RATES_KEY, $value)) {
+            throw new TransformationFailedException(sprintf(
+                'ExchangeRates reverse tranformation failed: key "%s" does not exists',
+                self::RATES_KEY
+            ));
+        }
+        
+        return $value[self::RATES_KEY];
     }
 }
