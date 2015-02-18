a3e74afb62c42d0b20140d915b4695e8d610053c
Dawid Drelichowski <dawid.drelichowski@gmail.com>
2015-02-18 13:21:58 +0100

    Form validation added

--- src/Kantor/Form/ExchangeRateType.php
+++ src/Kantor/Form/ExchangeRateType.php
@@ -3,15 +3,42 @@ namespace Kantor\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
 
 class ExchangeRateType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('country')
-            ->add('currency')
-            ->add('purchase', 'number')
-            ->add('sale', 'number')
+        $builder->add('country', 'text', array(
+            'attr' => array('maxlength' => 50),
+            'constraints' => array(
+                new NotBlank(),
+                new Length(array('max' => 50)),
+            )
+        ))
+            ->add('currency', 'text', array(
+                'attr' => array('maxlength' => 10),
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('max' => 10)),
+                )
+            ))
+            ->add('purchase', 'money', array(
+                'currency' => '',
+                'precision' => 4,
+                'constraints' => array(
+                    new Range(array('min' => 0))
+                )
+            ))
+            ->add('sale', 'money', array(
+                'currency' => '',
+                'precision' => 4,
+                'constraints' => array(
+                    new Range(array('min' => 0))
+                )
+            ))
             ->add('typeId', 'hidden');
     }
 
