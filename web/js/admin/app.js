6540f6f049c4c88c54526994f05fd845e8b3b64d
Dawid Drelichowski <dawid.drelichowski@gmail.com>
2015-02-24 16:16:44 +0100

    Firs version of add/remove rates in admin

--- web/js/admin/app.js
+++ web/js/admin/app.js
@@ -2,7 +2,50 @@
     'use strict';
 
     angular.module('ratesAdminApp', [])
-        .controller('ratesCtrl', function($templateCache) {
-            console.log($templateCache.get('wholesalePrototype.html'));
+        .controller('RatesController', function($scope) {
+            var rates = [];
+
+            $scope.$on('registerRate', function(event, data) {
+                rates.push(data);
+            });
+
+            $scope.remove = function(index) {
+                if (index in rates) {
+                    rates[index].$destroy();
+                    rates.slice(index, 1);
+                }
+            };
+
+            $scope.add = function() {
+                $scope.$broadcast('addRate', rates.length);
+            }
+        })
+        .directive('rates', function($compile, $templateCache) {
+            return {
+                restrict: 'A',
+                scope: true,
+                link: function(scope, element, attrs) {
+                    var type = angular.isDefined(attrs.rates) ? attrs.rates : '';
+
+                    scope.$on('addRate', function(event, index) {
+                        element.append($compile(
+                            $templateCache.get(type + 'Prototype.html').replace(/__name__/g, index)
+                        )(scope.$new()));
+                    });
+                }
+            }
+        })
+        .directive('rate', function() {
+            return {
+                restrict: 'A',
+                scope: {},
+                link: function(scope, element) {
+                    scope.$on('$destroy', function () {
+                        element.remove();
+                    });
+
+                    scope.$emit('registerRate', scope);
+                }
+            }
         });
 })(angular);
