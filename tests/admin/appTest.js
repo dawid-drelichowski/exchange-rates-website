5e6aaaca76c8b677b36143b8ceb522359286d5ed
Dawid Drelichowski <dawid.drelichowski@gmail.com>
2015-03-13 13:59:17 +0100

    JS RatesController rate removing tests

--- tests/admin/appTest.js
+++ tests/admin/appTest.js
@@ -2,20 +2,37 @@
     'use strict';
 
     describe('RatesControllerTest', function () {
-        var $scope;
+        var scope;
 
         beforeEach(function() {
             angular.mock.module('ratesAdminApp');
             angular.mock.inject(function($controller, $rootScope){
-                $scope = $rootScope.$new();
-                $controller('RatesController', {$scope: $scope});
+                scope = $rootScope.$new();
+                $controller('RatesController', {$scope: scope});
             });
         });
 
-        it('broadcast "addRate" event when rate added', function () {
-            spyOn($scope, '$broadcast').and.callThrough();
-            $scope.add();
-            expect($scope.$broadcast).toHaveBeenCalledWith('addRate', 0);
+        it('broadcasts "addRate" event when rate added', function () {
+            spyOn(scope, '$broadcast').and.callThrough();
+            scope.add();
+            expect(scope.$broadcast).toHaveBeenCalledWith('addRate', 0);
+        });
+
+        it('do not destroys rate when not existing rate removed', function () {
+            var rateScope = scope.$new();
+
+            spyOn(rateScope, '$destroy');
+            scope.remove(3);
+            expect(rateScope.$destroy.calls.any()).toBeFalsy();
+        });
+
+        it('destroys rate when existing rate removed', function () {
+            var rateScope = scope.$new();
+
+            spyOn(rateScope, '$destroy');
+            rateScope.$emit('registerRate', rateScope);
+            scope.remove(0);
+            expect(rateScope.$destroy.calls.count()).toEqual(1);
         });
     });
-})(describe, it, expect, beforeEach, spyOn,angular);
+})(describe, it, expect, beforeEach, spyOn, angular);
