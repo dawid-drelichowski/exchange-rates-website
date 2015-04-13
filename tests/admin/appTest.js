f5c5c1304a8b98566165fb278844fe8e28c06b20
Dawid Drelichowski <dawid.drelichowski@gmail.com>
2015-04-13 15:52:24 +0200

    JS rates  directive test added

--- tests/admin/appTest.js
+++ tests/admin/appTest.js
@@ -1,11 +1,11 @@
 (function(describe, it, expect, beforeEach, spyOn, angular) {
     'use strict';
 
-    describe('RatesControllerTest', function () {
+    describe('RatesController test', function () {
         var scope;
 
-        beforeEach(function() {
-            angular.mock.module('ratesAdminApp');
+        beforeEach(function () {
+            angular.mock.module('RatesAdminApp');
             angular.mock.inject(function($controller, $rootScope){
                 scope = $rootScope.$new();
                 $controller('RatesController', {$scope: scope});
@@ -33,6 +33,60 @@
             rateScope.$emit('registerRate', rateScope);
             scope.remove(0);
             expect(rateScope.$destroy.calls.count()).toEqual(1);
+        });
+    });
+
+    describe('Rates directive test', function() {
+        var type = {
+                name: 'retail',
+                id: 1
+            },
+            indexPlaceholder = '__name__',
+            templates = {
+                rates: '<div data-rates="' + type.name + '"></div>',
+                rate: '<div data-rate>' +
+                    '<a data-ng-click="remove(' + indexPlaceholder + ')">&times;</a>' +
+                    '<div>' +
+                        '<label for="form_' + type.name + '_' + indexPlaceholder + '_country" class="required">Country</label>' +
+                        '<input type="text" id="form_' + type.name + '_' + indexPlaceholder + '_country" name="form[' + type.name + '][' + indexPlaceholder + '][country]" required="required" maxlength="50" />' +
+                    '<div/>' +
+                    '<div>' +
+                        '<label for="form_' + type.name + '_' + indexPlaceholder + '_currency" class="required">Currency</label>' +
+                        '<input type="text" id="form_' + type.name + indexPlaceholder + 'currency" name="form[' + type.name + '][' + indexPlaceholder + '][currency]" required="required" maxlength="10" />' +
+                    '</div>' +
+                    '<div>' +
+                        '<label for="form_' + type.name + '_' + indexPlaceholder + '_purchase" class="required">Purchase</label>' +
+                        '<input type="text" id="form_' + type.name + '_' + indexPlaceholder + '_purchase" name="form[' + type.name + '][' + indexPlaceholder + '][purchase]" required="required" />' +
+                    '</div>' +
+                    '<div>' +
+                        '<label for="form_' + type.name + '_' + indexPlaceholder + '_sale" class="required">Sale</label>' +
+                        '<input type="text" id="form_' + type.name + '_' + indexPlaceholder + '_sale" name="form[' + type.name + '][' + indexPlaceholder + '][sale]" required="required" />' +
+                    '</div>' +
+                    '<input type="hidden" id="form_' + type.name + '_' + indexPlaceholder + '_typeId" name="form[' + type.name + '][' + indexPlaceholder + '][typeId]" value="' + type.id + '" />' +
+                '</div>'
+            },
+            scope,
+            element;
+
+        beforeEach(function () {
+            angular.mock.module('RatesAdminApp');
+            angular.mock.inject(function($compile, $rootScope, $templateCache){
+                scope = $rootScope.$new();
+                element = $compile(templates.rates)(scope);
+                $templateCache.put(type.name + 'Prototype.html', templates.rate);
+                scope.$digest();
+            });
+        });
+
+        it('adds new rate when "addRate" event broadcast', function () {
+            var index = 1,
+                elementContent;
+
+            scope.$broadcast('addRate', index);
+            elementContent = element.html();
+            expect(angular.element(elementContent).length).toEqual(1);
+            expect(elementContent).not.toContain(indexPlaceholder);
+            expect(elementContent).toContain(type.name);
         });
     });
 })(describe, it, expect, beforeEach, spyOn, angular);
--- web/js/admin/app.js
+++ web/js/admin/app.js
@@ -1,7 +1,7 @@
 (function(angular) {
     'use strict';
 
-    angular.module('ratesAdminApp', [])
+    angular.module('RatesAdminApp', [])
         .controller('RatesController', function($scope) {
             var rates = [];
 
